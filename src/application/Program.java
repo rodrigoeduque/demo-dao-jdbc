7d72b9803c55d7b1c00c6759ef19450dbbe5812f
Rodrigo Duque <rodrigoeduque@hotmail.com>
2020-08-18 12:07:18 -0300

    delete implementação

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -11,7 +12,8 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
-
+		
+		Scanner sc = new Scanner(System.in);
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println("========TESTE 1 : SELLER FINDBY ID ========");
@@ -47,6 +49,19 @@ public class Program {
 		seller.setName("Rodrigo Duque");
 		sellerDao.update(seller);
 		System.out.println("Update Finalizado");
+		
+		
+		System.out.println("\n=== TEST 6: Seller Delete=====");
+		System.out.println("Digite o ID a ser deletado : ");
+		
+		int id = sc.nextInt();
+		
+		sellerDao.deleteById(id);
+		
+		System.out.println("Delete completo");
+		sc.close();
+		
+		
 		
 	}
 
